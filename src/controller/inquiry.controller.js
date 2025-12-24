d6cb8a0805c7e0eba876bba9ba06b23b0c1ed00b
Sanjay Gawai <sanjay.gawai@rescueclick.com>
2025-12-24 16:04:15 +0530

    reject unauthorized

--- src/controller/inquiry.controller.js
+++ src/controller/inquiry.controller.js
@@ -34,9 +34,7 @@ export const inquiryFunction = async (req, res) => {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
             },
-            tls: {
-                rejectUnauthorized: false
-            }
+
         });
         
         const mailOptions = {
